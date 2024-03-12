cb479d0c5e63b1ef576140e84e91dff46c717578
Michael Kirschbaum <hi@michaelkirschbaum.com>
2024-03-12 04:23:28 -0600

    update pageCount

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-/*
-type FilterClauseType = {
-  id: string;
-  condition: 'equals' | 'does_not_equal' | 'greater_than' | 'less_than';
-  value: number | string;
-}
-
-type ResponseFiltersType = ResponseFilter[];
-*/
-
 app.get('/:formId/filteredResponses', async (req, res) => {
   const url = "https://api.fillout.com/v1/api/forms/" + req.params.formId + "/submissions?";
   const filters = JSON.parse(req.query.filters ? req.query.filters : null);
@@ -54,7 +44,11 @@ app.get('/:formId/filteredResponses', async (req, res) => {
       }) : data.responses;
     });
 
-    res.json({ ...data, responses: filteredData });
+    res.json({
+      responses: filteredData,
+      totalResponses: filteredData.length,
+      pageCount: Math.ceil(filteredData.length / req.param.limit ? req.param.limit : 150)
+    });
   } catch (err) {
     res.status(500).send('server error');
   }
