e5ba8681359c9ddd4206dd09579f7ea354c73975
Michael Kirschbaum <hi@michaelkirschbaum.com>
2024-03-12 17:58:25 -0600

    move index to src folder

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../../index');
+const app = require('../index');
 
 describe('filtererdResponses endpoint', () => {
   let formId = "cLZojxk94ous";
