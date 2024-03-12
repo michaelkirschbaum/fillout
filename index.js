09f0c610ce79d926e9d84032b1be0d0779683d5c
Michael Kirschbaum <hi@michaelkirschbaum.com>
2024-03-12 00:09:36 -0600

    add function passesFilters

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+/*
+type FilterClauseType = {
+  id: string;
+  condition: 'equals' | 'does_not_equal' | 'greater_than' | 'less_than';
+  value: number | string;
+}
+
+type ResponseFiltersType = ResponseFilter[];
+*/
+
+function passesFilters(response) {
+  response.questions.forEach((e) => {});
+
+  return true;
+}
 
 app.get('/:formId/filteredResponses', async (req, res) => {
   const url = "https://api.fillout.com/v1/api/forms/" + req.params.formId + "/submissions?";
@@ -19,9 +34,12 @@ app.get('/:formId/filteredResponses', async (req, res) => {
     const data = await response.json();
 
     // apply filters
-    filteredData = data.responses.filter((e) => e);
+    const filteredData = {
+      ...data,
+      responses: data.responses.filter(passesFilters)
+    };
 
-    res.json({ responses: filteredData, totalResponses: data.totalResponses, pageCount: data.pageCount });
+    res.json(filteredData);
   } catch (err) {
     res.status(500).send('server error');
   }
