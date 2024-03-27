f65fcbc74c6bc4ec5160adc4b78e9f681339730d
Michael Kirschbaum <hi@michaelkirschbaum.com>
2024-03-27 01:31:40 -0600

    lint

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,239 +1,239 @@
-const request = require('supertest');
-const app = require('../index');
+const request = require('supertest')
+const app = require('../index')
 
 const result = {
   responses: [
     {
 	  questions: [
 	    {
-		  id: "nameId",
+		  id: 'nameId',
 		  name: "What's your name?",
-		  type: "ShortAnswer",
-		  value: "Robert"
-		},
-		{
-		  id: "birthdayId",
-		  name: "What is your birthday?",
-		  type: "DatePicker",
-		  value: "1990-03-15T05:01:47.691Z"
-		},
-		{
-		  id: "employeesId",
-		  name: "How many employees work under you?",
-	      type: "number",
+		  type: 'ShortAnswer',
+		  value: 'Robert'
+        },
+        {
+		  id: 'birthdayId',
+		  name: 'What is your birthday?',
+		  type: 'DatePicker',
+		  value: '1990-03-15T05:01:47.691Z'
+        },
+        {
+		  id: 'employeesId',
+		  name: 'How many employees work under you?',
+	      type: 'number',
 	      value: 2
-		}
+        }
 	  ],
-	  submissionId: "abc",
-      submissionTime: "2024-03-27T22:20:05.324Z"
-	},
-	{
+	  submissionId: 'abc',
+      submissionTime: '2024-03-27T22:20:05.324Z'
+    },
+    {
       questions: [
         {
-          id: "nameId",
+          id: 'nameId',
     	  name: "What's your name?",
-          type: "ShortAnswer",
-          value: "Justin"
+          type: 'ShortAnswer',
+          value: 'Justin'
     	},
     	{
-    	  id: "birthdayId",
-    	  name: "What is your birthday?",
-    	  type: "DatePicker",
-    	  value: "1993-09-25T05:01:47.691Z"
-    	},
+    	  id: 'birthdayId',
+    	  name: 'What is your birthday?',
+    	  type: 'DatePicker',
+    	  value: '1993-09-25T05:01:47.691Z'
+    	}
       ],
-      submissionId: "123",
-      submissionTime: "2024-03-27T23:20:05.324Z"
+      submissionId: '123',
+      submissionTime: '2024-03-27T23:20:05.324Z'
     }
   ],
   totalResponses: 2,
   pageCount: 1
-};
+}
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve(result),
+    json: () => Promise.resolve(result)
   })
-);
+)
 
 beforeEach(() => {
-  fetch.mockClear();
-});
+  fetch.mockClear()
+})
 
 describe('filteredResponses endpoint', () => {
-  let formId = "testId";
+  const formId = 'testId'
 
   test('should return 200', async () => {
     const res = await request(app)
-      .get(`/${formId}/filteredResponses`);
+      .get(`/${formId}/filteredResponses`)
 
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual(result);
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual(result)
+    expect(fetch).toHaveBeenCalledTimes(1)
+  })
 
   test('should return 500', async () => {
-    fetch.mockImplementationOnce(() => Promise.reject());
+    fetch.mockImplementationOnce(() => Promise.reject())
     const res = await request(app)
-      .get(`/${formId}/filteredResponses`);
+      .get(`/${formId}/filteredResponses`)
 
-    expect(res.statusCode).toEqual(500);
-    expect(res.text).toEqual('server error');
-    expect(fetch).toHaveBeenCalledTimes(1);
-  });
+    expect(res.statusCode).toEqual(500)
+    expect(res.text).toEqual('server error')
+    expect(fetch).toHaveBeenCalledTimes(1)
+  })
 
   test('equals filter', async () => {
     const filters = [
       {
-        id: "nameId",
-        condition: "equals",
-        value: "Robert",
+        id: 'nameId',
+        condition: 'equals',
+        value: 'Robert'
       }
-    ];
+    ]
     const res = await request(app)
-      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters));
+      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters))
 
     expect(res.body).toEqual({
       responses: [
         {
     	  questions: [
     	    {
-    		  id: "nameId",
+    		  id: 'nameId',
     		  name: "What's your name?",
-    		  type: "ShortAnswer",
-    		  value: "Robert"
+    		  type: 'ShortAnswer',
+    		  value: 'Robert'
     		},
     		{
-    		  id: "birthdayId",
-    		  name: "What is your birthday?",
-    		  type: "DatePicker",
-    		  value: "1990-03-15T05:01:47.691Z"
+    		  id: 'birthdayId',
+    		  name: 'What is your birthday?',
+    		  type: 'DatePicker',
+    		  value: '1990-03-15T05:01:47.691Z'
     		},
     		{
-    		  id: "employeesId",
-    		  name: "How many employees work under you?",
-    	      type: "number",
+    		  id: 'employeesId',
+    		  name: 'How many employees work under you?',
+    	      type: 'number',
     	      value: 2
     		}
     	  ],
-    	  submissionId: "abc",
-          submissionTime: "2024-03-27T22:20:05.324Z"
+    	  submissionId: 'abc',
+          submissionTime: '2024-03-27T22:20:05.324Z'
     	}
       ],
       totalResponses: 1,
       pageCount: 1
-    });
-  });
+    })
+  })
 
   test('does not equal filter', async () => {
     const filters = [
       {
-        id: "employeesId",
-        condition: "does_not_equal",
-        value: 3,
+        id: 'employeesId',
+        condition: 'does_not_equal',
+        value: 3
       }
-    ];
+    ]
     const res = await request(app)
-      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters));
+      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters))
 
     expect(res.body).toEqual({
       responses: [
         {
     	  questions: [
     	    {
-    		  id: "nameId",
+    		  id: 'nameId',
     		  name: "What's your name?",
-    		  type: "ShortAnswer",
-    		  value: "Robert"
+    		  type: 'ShortAnswer',
+    		  value: 'Robert'
     		},
     		{
-    		  id: "birthdayId",
-    		  name: "What is your birthday?",
-    		  type: "DatePicker",
-    		  value: "1990-03-15T05:01:47.691Z"
+    		  id: 'birthdayId',
+    		  name: 'What is your birthday?',
+    		  type: 'DatePicker',
+    		  value: '1990-03-15T05:01:47.691Z'
     		},
     		{
-    		  id: "employeesId",
-    		  name: "How many employees work under you?",
-    	      type: "number",
+    		  id: 'employeesId',
+    		  name: 'How many employees work under you?',
+    	      type: 'number',
     	      value: 2
     		}
     	  ],
-    	  submissionId: "abc",
-          submissionTime: "2024-03-27T22:20:05.324Z"
+    	  submissionId: 'abc',
+          submissionTime: '2024-03-27T22:20:05.324Z'
     	}
       ],
       totalResponses: 1,
       pageCount: 1
-    });
-  });
+    })
+  })
 
   test('greater than filter', async () => {
     const filters = [
       {
-        id: "birthdayId",
-        condition: "greater_than",
-        value: "1991-01-01",
+        id: 'birthdayId',
+        condition: 'greater_than',
+        value: '1991-01-01'
       }
-    ];
+    ]
     const res = await request(app)
-      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters));
+      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters))
 
     expect(res.body).toEqual({
       responses: [
     	{
           questions: [
             {
-              id: "nameId",
+              id: 'nameId',
         	  name: "What's your name?",
-              type: "ShortAnswer",
-              value: "Justin"
+              type: 'ShortAnswer',
+              value: 'Justin'
         	},
         	{
-        	  id: "birthdayId",
-        	  name: "What is your birthday?",
-        	  type: "DatePicker",
-        	  value: "1993-09-25T05:01:47.691Z"
-        	},
+        	  id: 'birthdayId',
+        	  name: 'What is your birthday?',
+        	  type: 'DatePicker',
+        	  value: '1993-09-25T05:01:47.691Z'
+        	}
           ],
-          submissionId: "123",
-          submissionTime: "2024-03-27T23:20:05.324Z"
+          submissionId: '123',
+          submissionTime: '2024-03-27T23:20:05.324Z'
         }
       ],
       totalResponses: 1,
       pageCount: 1
-    });
-  });
+    })
+  })
 
   test('less than filter', async () => {
     const filters = [
       {
-        id: "birthdayId",
-        condition: "less_than",
-        value: "2024-01-01",
+        id: 'birthdayId',
+        condition: 'less_than',
+        value: '2024-01-01'
       }
-    ];
+    ]
     const res = await request(app)
-      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters));
+      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters))
 
-    expect(res.body).toEqual(result);
-  });
+    expect(res.body).toEqual(result)
+  })
 
   test('invalid filter name returns nothing', async () => {
     const filters = [
       {
-        id: "nameId",
-        condition: "equal_to",
-        value: "Robert",
+        id: 'nameId',
+        condition: 'equal_to',
+        value: 'Robert'
       }
-    ];
+    ]
     const res = await request(app)
-      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters));
+      .get(`/${formId}/filteredResponses?filters=` + JSON.stringify(filters))
 
     expect(res.body).toEqual({
       responses: [],
       totalResponses: 0,
       pageCount: 0
-    });
+    })
   })
-});
+})
--- src/index.js
+++ src/index.js
@@ -1,56 +1,56 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
 
 app.get('/:formId/filteredResponses', async (req, res) => {
-  const url = "https://api.fillout.com/v1/api/forms/" + req.params.formId + "/submissions?";
-  const filters = JSON.parse(req.query.filters ? req.query.filters : null);
+  const url = 'https://api.fillout.com/v1/api/forms/' + req.params.formId + '/submissions?'
+  const filters = JSON.parse(req.query.filters ? req.query.filters : null)
 
   try {
     const response = await fetch(url + new URLSearchParams({ ...req.query }), {
-      method: "GET",
-      mode: "cors",
+      method: 'GET',
+      mode: 'cors',
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.API_KEY}`
-      },
-    });
-    const data = await response.json();
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.API_KEY}`
+      }
+    })
+    const data = await response.json()
 
     // remove response if every filter condition is not met
     const filteredData = data.responses?.filter(res => {
       return filters?.every(filter => {
         // get matching question from the filter id
-        const entry = res.questions.find(e => e.id === filter.id);
-        if (!entry) return false;
+        const entry = res.questions.find(e => e.id === filter.id)
+        if (!entry) return false
 
         // if condition not met return false
-        switch(filter.condition) {
+        switch (filter.condition) {
           case 'equals':
-            return entry?.value === filter.value ? true : false;
+            return entry?.value === filter.value
           case 'does_not_equal':
-            return entry?.value !== filter.value ? true : false;
+            return entry?.value !== filter.value
           case 'greater_than':
-            return entry?.value > filter.value && !isNaN(new Date(entry.value)) && !isNaN(new Date(filter.value)) ? true : false;
+            return !!(entry?.value > filter.value && !isNaN(new Date(entry.value)) && !isNaN(new Date(filter.value)))
           case 'less_than':
-            return entry?.value < filter.value && !isNaN(new Date(entry.value)) && !isNaN(new Date(filter.value)) ? true : false;
+            return !!(entry?.value < filter.value && !isNaN(new Date(entry.value)) && !isNaN(new Date(filter.value)))
           default:
-            return false;
-        }  // if filters are not defined return unfiltered responses
-      }) ?? data.responses;
-    }) ?? data;
+            return false
+        } // if filters are not defined return unfiltered responses
+      }) ?? data.responses
+    }) ?? data
 
     res.json({
       responses: filteredData,
       totalResponses: filteredData.length,
       pageCount: Math.ceil(filteredData.length / (req.param.limit ?? 150))
-    });
+    })
   } catch (err) {
-    res.status(500).send('server error');
+    res.status(500).send('server error')
   }
-});
+})
 
-app.listen(port, "0.0.0.0", () => console.log(`listening on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`listening on port ${port}`))
 
-module.exports = app;
+module.exports = app
